f6ef1e0add65db6a98f36a70dcab6e91145078ea
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-16 23:26:33 -0600

    1:26

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -9,21 +9,37 @@ class PostRoutes {
 		this.routes()
 	}
 
-	async getPosts(req: Request, res: Response) {
+	public async getPosts(req: Request, res: Response): Promise<void> {
 		const posts = await PostModel.find()
 		res.json(posts)
 	}
 
-	async getPost(req: Request, res: Response) {}
-
-	createPost(req: Request, res: Response) {
-		console.log(req.body)
-		res.json('data received')
+	public async getPost(req: Request, res: Response): Promise<void> {
+		const post = await PostModel.findOne({ url: req.params.url })
+		res.json(post)
 	}
 
-	updatePost(req: Request, res: Response) {}
+	public async createPost(req: Request, res: Response): Promise<void> {
+		console.log(req.body)
+		const { title, url, content, image } = req.body
+		const newPost = new PostModel({ title, url, content, image })
+		await newPost.save()
+		res.json({ data: newPost })
+	}
 
-	deletePost(req: Request, res: Response) {}
+	public async updatePost(req: Request, res: Response): Promise<void> {
+		const { url } = req.params
+		const Post = await PostModel.findOneAndUpdate({ url }, req.body, {
+			new: true,
+		})
+		res.json(Post)
+	}
+
+	public async deletePost(req: Request, res: Response): Promise<void> {
+		const { url } = req.params
+		await PostModel.findOneAndDelete({ url })
+		res.json({ response: 'Post Deleted successfully' })
+	}
 
 	routes() {
 		this.router.get('/', this.getPosts)
